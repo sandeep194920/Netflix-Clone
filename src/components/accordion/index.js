ae5901719776538ab26d16d4f43f29eff2b09a64
sandeep194920 <sandeepamaranath@gmail.com>
2020-10-25 18:21:05 -0400

    built accordion for faqs

---
+++ src/components/accordion/index.js
@@ -0,0 +1,56 @@
+import React, { useState, useContext, createContext } from "react";
+import {
+  Container,
+  Title,
+  Body,
+  Item,
+  Header,
+  Inner,
+} from "./styles/accordion";
+const ToggleContext = createContext();
+
+export default function Accordion({ children, ...restProps }) {
+  return (
+    <Container {...restProps}>
+      <Inner>{children}</Inner>
+    </Container>
+  );
+}
+
+Accordion.Title = function AccordionTitle({ children, ...restProps }) {
+  return <Title {...restProps}>{children}</Title>;
+};
+
+Accordion.Item = function AccordionItem({ children, ...restProps }) {
+  // item is the entire block of a single faq that contains header (question) and the body (answer). Body opens and closes on clicking on header. So we pass context to body to header
+  const [toggleShow, setToggleShow] = useState(false);
+
+  return (
+    <ToggleContext.Provider value={{ toggleShow, setToggleShow }}>
+      <Item {...restProps}>{children}</Item>
+    </ToggleContext.Provider>
+  );
+};
+
+Accordion.Header = function AccordionHeader({ children, ...restProps }) {
+  const { toggleShow, setToggleShow } = useContext(ToggleContext);
+  return (
+    <Header
+      onClick={() => setToggleShow((toggleShow) => !toggleShow)}
+      {...restProps}
+    >
+      {children}
+      {toggleShow ? (
+        <img src="/images/icons/close-slim.png" alt="Close" />
+      ) : (
+        <img src="/images/icons/add.png" alt="Open" />
+      )}
+    </Header>
+  );
+};
+
+Accordion.Body = function AccordionBody({ children, ...restProps }) {
+  const { toggleShow } = useContext(ToggleContext);
+
+  return toggleShow && <Body {...restProps}>{children}</Body>;
+};
