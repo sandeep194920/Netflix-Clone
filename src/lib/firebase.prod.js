2917a251e367f02ea736d7d07d614c5da4dfd55d
sandeep194920 <sandeepamaranath@gmail.com>
2020-10-28 11:47:27 -0400

    setting up firebase configs and firebase context

---
+++ src/lib/firebase.prod.js
@@ -0,0 +1,20 @@
+import Firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+// used only once to seed the data to firebase
+// import { seedDatabase } from "../seed";
+
+// we need to put the fb configs here. We get this from .env.local which is not pushed on github.
+const config = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGE_SENDER_ID,
+  appId: process.envREACT_APP_APP_ID,
+};
+const firebase = Firebase.initializeApp(config);
+// This must be done (uncommented) only once (the first time) and never again since the data might get duplicated. First time, after uncommenting this we export firebase through this file using index.js
+// seedDatabase(firebase);
+export { firebase };
