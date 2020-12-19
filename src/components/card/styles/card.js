6c5d57a8e1f650fd2c56d602109a853753e33910
sandeep194920 <sandeepamaranath@gmail.com>
2020-12-19 11:25:13 -0500

    deploying

--- src/components/card/styles/card.js
+++ src/components/card/styles/card.js
@@ -71,13 +71,13 @@ export const Meta = styled.div`
 `;
 
 export const Image = styled.img`
-  border: 0;
   width: 100%;
   max-width: 305px;
   cursor: pointer;
   height: auto;
   padding: 0;
   margin: 0;
+  border: 0;
 `;
 
 export const Item = styled.div`
